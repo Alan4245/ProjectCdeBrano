2132d412089e860490de9d44e2f2ae80facf6aa0
Alan4245 <alanminecraft77@gmail.com>
2020-10-28 11:46:32 +0100

    Creazione del Progetto

---
+++ ProjectCdeBrani/Brano.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCdeBrani
+{
+    class Brano
+    {
+
+        /* OMICINI: creare classe brano che abbia le seguenti proprietà: titolo(string),autore(string),durata(int) (la durata viene rappresentata in min) e cd(string)
+         * la proprietà del titolo deve avere un get e set: il get ritorna il valore di titolo e il set controlla che il valore NON sia nullo,se è nulla lancia un errore altrimenti assegnare il valore
+         * alla variabile titolo.
+         * L'autore funziona esattamente come il titolo
+         * La durata funziona come il titolo ma con diverso controllo sul set: se il valore che arriva è negativo o uguale a 0 lancio un errore,diversamente assegno il valore alla variabile durata.
+         * cd nessun controllo per il set
+         * Ricorsa di creare gli attributi PRIVATI e le proprietà pubbliche
+         * Il costruttore è 1 solo e si aspetta tutti e 3 i parametri.
+         * Metodo shortSong: verifica se il valore che gli arriva è maggiore o uguale della durata del brano,se è vero ritorna falso,se è falso ritorna vero.
+         * Fare l'override del Tostring in modo che il toString ritorni questo tipo di stringa: "TITOLO|AUTORE|DURATA|CD"
+        */
+        
+    }
+}
---
+++ ProjectCdeBrani/Cd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCdeBrani
+{
+    class Cd
+    {
+
+        /* NAPOLITANO: creare la classe Cd che abbia i seguenti attributi: titoloCd(string),autoreCd(string),List<Brano> listaBrani (lista di oggetti Brano)
+         * Costruttore: 1 solo costruttore che si aspetta titoloCd e autoreCd
+         * Proprietà di titoloCd e autoreCd in cui il get restituisce la variabile interessata e il set che si preoccupa di controllare se il valore che arriva è nullo,
+         * se fosse nullo lancia un errore altrimenti assegna il valore alle rispettive variabili.
+         * Metodo listaBrani che restituisce una lista di oggetti string che sono assegnati dai vari titoli dei brani
+         * Metodo durataTot che restituisce la somma di tutte le durate dei brani presenti
+         * Metodo aggiungiBrano che si aspetta un oggetto di tipo Brano e lo aggiunge alla listaBrani.
+         * 
+         * 
+         * 
+         * 
+        */
+    }
+}

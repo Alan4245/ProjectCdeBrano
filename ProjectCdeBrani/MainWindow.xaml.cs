943ce229786e898fedeaae90714fbe4c6de136f6
Alan4245 <alanminecraft77@gmail.com>
2020-11-04 12:03:24 +0100

    Creato il mainwindow + cs

--- ProjectCdeBrani/MainWindow.xaml.cs
+++ ProjectCdeBrani/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +21,54 @@ namespace ProjectCdeBrani
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private List<Brano> listaBrani;
+        private List<Cd> listaCd;
+
+        
         public MainWindow()
         {
             InitializeComponent();
+            listaBrani = new List<Brano>();
+            listaCd = new List<Cd>();
+
+
         }
 
 
-        //PARTE DI CODICE IN SVILUPPO DA MARIANI IN SEGUITO LA RICEZIONE DELLE CLASSI CD E BRANO.
+
+        private void caricaBraniECd()
+        {
+            //TITOLO|AUTORE|DURATA|CD
+            using(StreamReader sr = new StreamReader("brani.txt"))
+            {
+
+                string line;
+                string titolo;
+                string autore;
+                int durata;
+                string cd;
+
+                do
+                {
+
+                    line = sr.ReadLine();
+                    string[] lineSplittata = line.Split('|');
+                    titolo = lineSplittata[0];
+                    autore = lineSplittata[1];
+                    durata = int.Parse(lineSplittata[2]);
+                    cd = lineSplittata[3];
+
+                    //Chiamo il costruttore di Brano
+
+
+                } while (sr.EndOfStream);
+
+            }
+
+
+
+        }
 
     }
 }
